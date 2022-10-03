faa2794492ab59f76dd281aaa0bf3bed00783a84
Sherman <95594806+GermanSchmitz@users.noreply.github.com>
2022-10-03 00:42:04 -0300

    Refactorazacion de Clases, paso previo a persistencia

--- src/Veterinaria.js
+++ src/Veterinaria.js
@@ -5,12 +5,12 @@ import Familiar from "./Familiar.js"
 export default class Veterinaria {
     #nombre
     #registroFamiliares
-    #mascotas
+    #registroMascotas
     #fechas
 
     constructor( nombre ) {
         this.#nombre = nombre
-        this.#mascotas = []
+        this.#registroMascotas = new RegistroMascotas()
         this.#registroFamiliares = new RegistroFamiliares()
         this.#fechas = []
     }
@@ -19,13 +19,13 @@ export default class Veterinaria {
 
         if ( this.turnoDisponible( fecha, hora ) ) {
             const idMascota = `${ dniFamiliar }-${ nombreMascota }`
-            let mascota = this.#mascotas.find( m => m.id === idMascota )
+            let mascota = this.#registroMascotas.buscarPorId( idMascota )
             let familiar = this.#registroFamiliares.buscarPorDni( dniFamiliar )
 
             if ( !mascota ) {
                 try {
                     mascota = new Mascota( idMascota, nombreMascota, '', '', 0, 0 )
-                    // Agregar a la lista
+                    this.#registroMascotas.registar( mascota )
                 } catch ( e ) {
                     console.log( e )
                 }
@@ -70,7 +70,7 @@ export default class Veterinaria {
     completarRegistro( { idMascota, nombreMascota, razaMascota, fNacMascota, edadMascota, pesoMascota },
                        { dniFamiliar, nombreFamiliar, apellidoFamiliar, emailFam, telFamiliar, direccionFamiliar } ) {
 
-        let mascota = this.#mascotas.find( m => m.id === idMascota )
+        let mascota = this.#registroMascotas.buscarPorId( idMascota )
         let familiar = this.#registroFamiliares.buscarPorDni( dniFamiliar )
 
         if ( !familiar ) {
@@ -83,7 +83,7 @@ export default class Veterinaria {
             if ( !mascota ) {
                 try {
                     mascota = new Mascota( idMascota, nombreMascota, razaMascota, fNacMascota, edadMascota, pesoMascota )
-                    // Agregar Mascota
+                    this.#registroMascotas.registar( mascota )
                 } catch ( error ) {
                     console.log( error )
                 }
@@ -111,7 +111,7 @@ export default class Veterinaria {
 
     eliminarRegistro( mascota ) {
         const edadMayor = 25
-        let mascotaBuscada = this.#mascotas.find( m => m.id === mascota.id )
+        let mascotaBuscada = this.#registroMascotas.buscarPorId( mascota.id )
         if ( mascotaBuscada ) {
             if ( mascotaBuscada.edad > edadMayor )
                 this.#mascotas.splice( mascotaBuscada.id, 1 )
@@ -120,7 +120,7 @@ export default class Veterinaria {
     }
 
     modificarDatosDeLaMascota( idMascota, mascota ) {
-        let mascotaBuscada = this.#mascotas.find( m => m.id === idMascota );
+        let mascotaBuscada = this.#registroMascotas.buscarPorId( mascota.id )
         if ( mascotaBuscada ) {
             mascotaBuscada.modificarDatosMascota( mascota );
         } else {
